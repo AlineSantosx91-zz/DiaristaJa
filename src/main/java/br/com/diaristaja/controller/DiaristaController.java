b88a726bebb9ab246c07dc1ee607288dd38cec8d
ALINE RODRIGUES DOS SANTOS <aline.rsantos12@senacsp.edu.br>
2017-05-16 21:44:07 -0300

    Troca nome dos pacotes

--- src/main/java/br/com/diaristaja/controller/DiaristaController.java
+++ src/main/java/br/com/diaristaja/controller/DiaristaController.java
@@ -1,4 +1,4 @@
-package controller;
+package br.com.diaristaja.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
