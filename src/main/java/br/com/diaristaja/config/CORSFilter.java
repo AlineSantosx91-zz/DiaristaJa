5aeb14731cc5670297b5c507f76aec4bb06db76d
Aline Santos <aline.santos@concrete.com.br>
2017-06-10 15:58:53 -0300

    adiciona multiplas urls cors

--- src/main/java/br/com/diaristaja/config/CORSFilter.java
+++ src/main/java/br/com/diaristaja/config/CORSFilter.java
@@ -1,19 +1,18 @@
 package br.com.diaristaja.config;
 
-//@Configuration
-public class CORSFilter {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-//	@Bean
-//	public FilterRegistrationBean corsFilter() {
-//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//		CorsConfiguration config = new CorsConfiguration();
-//		config.setAllowCredentials(true);
-//		config.addAllowedOrigin("*");
-//		config.addAllowedHeader("*");
-//		config.addAllowedMethod("*");
-//		source.registerCorsConfiguration("/**", config);
-//		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-//		bean.setOrder(0);
-//		return bean;
-//	}
+@Configuration
+@EnableWebMvc
+public class CORSFilter extends WebMvcConfigurerAdapter{
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://localhost:4200", "http://nataliabraz.com")
+			.allowedMethods("PUT", "DELETE", "POST", "GET");
+	}
 }
